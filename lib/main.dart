7618f8d76225a1a871ff93f1f535551b757e857b
SakunPanthi123 <spanthi863@rku.ac.in>
2023-08-25 22:11:14 +0530

    minor edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp2/firebase_options.dart';
