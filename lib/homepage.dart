62756d7a7f3cee78409b2a6bd99211bf64a4f6af
SakunPanthi123 <spanthi863@rku.ac.in>
2023-08-25 21:25:45 +0530

    Removed unnecessary addToDoPage function

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -14,7 +14,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   CollectionReference get toDoPage => API.firestore.collection('toDoPages');
-  void addToDoPage() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
